753b8ebb62181089b2fbd21e77b0c5ef52531c51
Gavin T <gavintranquilino@gmail.com>
2024-02-21 13:19:35 -0500

    CMakeLists.txt at root and creates bin folder

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
-    std::cout << "I spent 4+ hours trying to configure CMake and understand it btw\n";
+    std::cout << "I spent 4+ hours trying to configure CMake and understand it btw\n"
+              << "bin folder created? maybe?";
     return 0;
 }
