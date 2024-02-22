fd49413def554dca13f2acb597370e67635ae00e
Gavin T <gavintranquilino@gmail.com>
2024-02-22 12:12:51 -0500

    1st working model of adding raylib through git submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <raylib.h>
 #include <bruh.h>
 
 int main()
 {
+    InitWindow(800, 600, "Raylib Window");
+
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     std::cout << "I spent 4+ hours trying to configure CMake and understand it btw\n"
               << "bin folder created? maybe?\n"
               << bruh::nah() << '\n';
