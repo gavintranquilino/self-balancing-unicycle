b7af947e92ff6a9da7abcf218d87d57050cf7110
Gavin T <gavintranquilino@gmail.com>
2024-02-25 13:48:23 -0500

    Theta @ 0 is up

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,12 @@ int main()
     const int SCREEN_HEIGHT = 450;
 
     // MMGS, millimeters, grams, seconds
-    double timeInterval = 0.1;
-    double massBase = 50;
-    double massPendulum = 20;
-    double lengthPendulum = 50;
+    double timeInterval = 0.01;
+    double massBase = 5;
+    double massPendulum = 3;
+    double lengthPendulum = 5;
     double xPos = SCREEN_WIDTH/ 2;
-    double angle = PI / 4; // Radians
+    double angle = PI/4; // Radians
     double xVel = 0;
     double angleVel = 0;
     double xAccel = 0;
@@ -35,8 +35,8 @@ int main()
 
     // ----- Visual Representation Variables -----
     int yPos = 200;
-    int cartWidth = 50;
-    int cartHeight = 20;
+    int cartWidth = 100;
+    int cartHeight = 50;
 
     // ----- Main game loop -----
     while (!WindowShouldClose()) 
@@ -46,15 +46,13 @@ int main()
     
         // change inverted pendulum x position with arrow keys
         if (IsKeyDown(KEY_RIGHT)) 
-        {
-            // pendulum.setXPos(pendulum.getXPos() + 10);
-            appliedForce += 1;
-        }
+            pendulum.setXPos(pendulum.getXPos() + 10);
         if (IsKeyDown(KEY_LEFT)) 
-        {
-            // pendulum.setXPos(pendulum.getXPos() - 10);
-            appliedForce -= 1;
-        }
+            pendulum.setXPos(pendulum.getXPos() - 10);
+        if (IsKeyDown(KEY_UP)) 
+            pendulum.setAngle(pendulum.getAngle() + (PI/12));
+        if (IsKeyDown(KEY_DOWN)) 
+            pendulum.setAngle(pendulum.getAngle() - (PI/12));
 
         // Draw the cart
         DrawRectangle(pendulum.getXPos() - cartWidth / 2, yPos, cartWidth, cartHeight, BLACK);
@@ -62,17 +60,30 @@ int main()
         // Draw the pendulum
         DrawLine(
                 pendulum.getXPos(), yPos, // start line at base of cart
-                pendulum.getXPos() + pendulum.getLengthPendulum() * cos(pendulum.getAngle()),
-                yPos - pendulum.getLengthPendulum() * sin(pendulum.getAngle()), 
+
+                // Get the end of the pendulum (updated)
+                pendulum.getXPos() + ((pendulum.getLengthPendulum() + 50) * sin(pendulum.getAngle())),
+                yPos - (pendulum.getLengthPendulum() + 50) * cos(pendulum.getAngle()), 
+
                 RED
                 );
-        
+      
         pendulum.update(timeInterval);
-        appliedForce = 0;
+        // appliedForce = 0;
 
         // print mouse position
         std::cout << GetMouseX() << ' ' << GetMouseY() << '\n';
         // DrawCircle(GetMouseX(), GetMouseY(), 2, RED);
+
+        // print all the pendulum member variables to the window with a label on what the value is
+        DrawText(("xPos: " + std::to_string(pendulum.getXPos())).c_str(), 10, 10, 10, BLACK);
+        DrawText(("angle: " + std::to_string(pendulum.getAngle())).c_str(), 10, 20, 10, BLACK);
+        DrawText(("xVel: " + std::to_string(pendulum.getXVel())).c_str(), 10, 30, 10, BLACK);
+        DrawText(("angleVel: " + std::to_string(pendulum.getAngleVel())).c_str(), 10, 40, 10, BLACK);
+        DrawText(("xAccel: " + std::to_string(pendulum.getXAccel())).c_str(), 10, 50, 10, BLACK);
+        DrawText(("angleAccel: " + std::to_string(pendulum.getAngleAccel())).c_str(), 10, 60, 10, BLACK);
+        DrawText(("timeElapsed: " + std::to_string(pendulum.getTimeElapsed())).c_str(), 10, 70, 10, BLACK);
+        DrawText(("appliedForce: " + std::to_string(appliedForce)).c_str(), 10, 80, 10, BLACK);
 
         EndDrawing();
     }
